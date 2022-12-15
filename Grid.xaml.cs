055d66d6aa21a324eb11bd9600e23fb7e7a126ba
Loro2060 <102278322+Loro2060@users.noreply.github.com>
2022-12-14 23:55:13 -0500

    Add files via upload

---
+++ Grid.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Runtime.InteropServices.ComTypes;
+using Xamarin.Forms.PlatformConfiguration;
+
+
+
+namespace App3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Grid : ContentPage
+    {
+        public Grid(string Nombre, string Edad, string Pais, string Num, List<String> Ocup)
+        {
+            InitializeComponent();
+
+            Nombre1.Text = Nombre;
+            Edad1.Text = Edad;
+            Procedencia.Text = Pais;
+            Numero.Text = Num;
+
+            foreach (var i in Ocup)
+            {
+                var label = new Label { Text = i };
+                Ocupaciones.Children.Add(label);
+            }
+
+        }
+
+        void GenerarEmail(object sender, System.EventArgs e)
+        {
+            List<string> toAddress = new List<string>();
+            string cuerpo = Nombre1.Text + Edad1.Text + Procedencia.Text + Numero.Text;
+            toAddress.Add("jacintopichimawido25@gmail.com");
+            EnviarEmail("El CV", cuerpo, toAddress);
+        }
+
+        private async void EnviarEmail(string subject, string body, List<string> gente)
+        {
+            var message = new EmailMessage
+            {
+                Subject = subject,
+                Body = body,
+                To = gente,
+            };
+            await Email.ComposeAsync(message);
+        }
+            
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            string nombre = Nombre.Text;
+            string edad = Edad.Text;
+            string pais = Pais.Text;
+            string numero = Numero.Text;
+
+            List<String> Ocupaciones = new List<String>();
+
+            var Ocup = ocupacion.Children.ToList();
+
+            foreach (var i in Ocup)
+            {
+                var myEntry = i as Entry;
+                Ocupaciones.Add(myEntry.Text);
+            }
+
+            Application.Current.MainPage.Navigation.PushModalAsync(new Grid(nombre, edad, pais, numero, Ocupaciones), true);
+        }
+        private void Button_Ocupacion(object sender, EventArgs e)
+        {
+            var entry = new Entry() {};
+            ocupacion.Children.Add(entry);
+
+        }
+
+    }
+}
